7f7054aa3e9caf7f05c59bd78cc49e431bd61133
nranjan54 <ranjan.k.niraj@gmail.com>
2016-03-10 00:51:30 +0530

    Added .gitignore and some files

--- general.py
+++ general.py
@@ -5,3 +5,42 @@ def createProjectDir(directory):
 	if not os.path.exists(directory):
 		print('Creating project ' + directory)
 		os.makedirs(directory)
+
+#Create queue and crawled files (if not created)
+def createDataFiles(projectName, baseUrl):
+	queue = projectName + '/queue.txt'
+	crawled = projectName + '/crawled.txt'
+	if not os.path.isfile(queue):
+		writeFile(queue, baseUrl)
+	if not os.path.isfile(crawled):
+		writeFile(crawled, '')
+
+#Create a new file
+def writeFile(path, data):
+	f = open(path, 'w')
+	f.write(data)
+	f.close()
+
+#Add data onto an existing file
+def appendToFile(path, data):
+	with open(path, 'a') as file:
+		file.write(data + '\n')
+
+#Delete the contents of the file
+def deleteFileContents(path):
+	with open(path, 'w'):
+		pass
+
+#Read a file and convert each line to a set items
+def fileToSet(fileName):
+	results = set()
+	with open(fileName, 'rt') as f:
+		for line in f:
+			results.add(line.replace('\n', ''))
+	return results
+
+#Iterate through a set, each item will be a new line in the file
+def setToFile(links, file):
+	deleteFileContents(file)
+	for link in sorted(links):
+		appendToFile(file, link)
